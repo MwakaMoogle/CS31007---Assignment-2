d678b2ac217eb5fb6bd5d3b8d026579ed186def0
Dylan <Dylanmwakasekele@gmai.com>
2026-02-26 15:58:18 +0000

    Added an example test

--- tests/test_FR-01.py
+++ tests/test_FR-01.py
@@ -1,6 +1,9 @@
 # FR-01: System must allow user to create different questions with different formats
-import pytest
 
 # Test1.1: Create a question default question object
 def test_create_default_question():
-    pass
+    from question import Question
+    q = Question()
+    assert q.questionText == ""
+    assert q.questionAnswer == Answer()
+    assert q.scoringStrategy == "default"
