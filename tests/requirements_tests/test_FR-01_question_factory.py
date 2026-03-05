a7e885059706787a7fa6fb4f111793acf689d374
MwakaMoogle <dylanmwakasekele@gmmail.com>
2026-03-05 15:35:06 +0000

    Created test to test when a TextQuestion Object is requested

--- tests/requirements_tests/test_FR-01_question_factory.py
+++ tests/requirements_tests/test_FR-01_question_factory.py
@@ -18,6 +18,34 @@ class TestQuestionFactory:
             )
 
         # Assert
-        assert isinstace(question, MultipleChoiceQuestion), "Factory did nor return a MultipleChoiceQuestion"
+        assert isinstance(question, MultipleChoiceQuestion), "Factory did not return a MultipleChoiceQuestion"
         assert question.text == "What is the capital of France?", "Question text was not set correctly"
         assert question.correctAnswer == "Paris", "Correct answer was not set correctly"
+
+    def test_factory_creates_text_question(self):
+        """
+        Tests that requestion a "Text" question return the correct object.
+        """
+        factory = QuestionFactory()
+
+        # Create the question object and popluate it 
+        question = factory.createQuestion(
+                type = "Text",
+                text = "What is 5 + 5?",
+                answer = "10"
+                )
+
+        # Assert
+        assert isinstance(question, MultipleChoiceQuestion), "Factory did not return a TextQuestion"
+        assert question.text == "Whatis 5 + 5?", "Question text was not set correctly"
+        assert question.correctAnswer == "10", "Correct answer was not set correctly"
+
+    def test_factory_raises_an_error_for_invalid_type(self):
+        """
+        Tests that requesting an invalid question object type raises an errror
+        """
+
+        factory = QuestionFactory()
+
+        # Create a question object and populate it
+        question = factory.createQuestion()
