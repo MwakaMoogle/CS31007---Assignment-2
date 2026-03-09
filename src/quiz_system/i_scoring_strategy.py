065c9929f320b811bc341363b2b6d020afcfaeaf
MwakaMoogle <dylanmwakasekele@gmmail.com>
2026-03-09 13:31:55 +0000

    Created the IScoringStratergy Interface class
    Made the interface and vaious classe's using it with diffent scorring
    aproaches. Untested.

--- src/quiz_system/i_scoring_strategy.py
+++ src/quiz_system/i_scoring_strategy.py
@@ -1,3 +1,40 @@
-class IScoringStrategy:
+from abc import ABC, abstractmethod
+
+class IScoringStrategy(ABC):
+
+    @abstactmethod
+    def executeScoring(isCorrect, difficulty):
+        """
+        Define how points are awarderd
+        """
+        pass
+
+class StandardScore(IscoringStrategy):
+    
+    def executeScoring(isCorrect, difficulty):
+        if iscorrect:
+            return 1
+        else: 
+            return 0
+    
+class HardScore(IScoringStrategy):
+
+    def executeScoring(isCorrect, difficulty):
+        if isCorrect:
+            return 5
+        else:
+            return 0
+
+class PenaltyScore(IScoringStrategy):
+    
+    def executeScoring(isCorrect, difficulty):
+        if isCorrect:
+            return -1
+        else:
+            return 0
+
+class BonusScore(IScoringStrategy):
     pass
+
+
 
