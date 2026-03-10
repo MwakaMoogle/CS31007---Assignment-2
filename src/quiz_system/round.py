5e4c094a9d1ba4822defd095d842620011b8a590
MwakaMoogle <dylanmwakasekele@gmmail.com>
2026-03-10 15:55:43 +0000

    2 out of 5 tests are now passing
    Had to do a lot of remainign / formating

--- src/quiz_system/round.py
+++ src/quiz_system/round.py
@@ -1,16 +1,15 @@
 class Round:
-    def _init_(self, category_title):
+    def __init__(self, category_title):
         self.category_title = category_title
         self.questions = []
 
-        def add_question(self, question):
-                self.questions = self.questions + [question]
+    def add_question(self, question):
+        self.questions = self.questions + [question]
 
-        def get_question(self, index):
-            if not isinstance(index, int):
-                 raise TypeError("The index must be an integer")
-            if index < 0 or index>=len(self.questions):
-                 raise IndexError("Enter a question that exists")
-            
+    def get_question(self, index):
+        if not isinstance(index, int):
+            raise TypeError("The index must be an integer")
+        if index < 0 or index>=len(self.questions):
+            raise IndexError("Enter a question that exists")
 
-            return self.questions[index]
+        return self.questions[index]
--- tests/requirements_tests/test_FR-02_rounds.py
+++ tests/requirements_tests/test_FR-02_rounds.py
@@ -2,7 +2,7 @@ import pytest
 from quiz_system import QuestionFactory, Round
 
 class TestRounds:
-    def test_adding_question_to_round(self):
+    def test_adding_question_to_quiz_round(self):
         factory = QuestionFactory()
         question = factory.create_question(
                 q_type="MultipleChoice",
@@ -10,16 +10,16 @@ class TestRounds:
                 correct_answer="Paris",
                 possible_answers = ["London", "Paris", "Berlin", "Madrid"]
                 )
-        round = Round(
+        quiz_round = Round(
                 category_title="Geography"
                )
-        num_questions_in_round = len(round.questions)
-        round.add_question(question)
+        num_questions_in_quiz_round = len(quiz_round.questions)
+        quiz_round.add_question(question)
 
-        assert question in round.questions
-        assert len(round.questions) == num_questions_in_round + 1
+        assert question in quiz_round.questions
+        assert len(quiz_round.questions) == num_questions_in_quiz_round + 1
 
-    def test_adding_multiple_questions_to_round(self):
+    def test_adding_multiple_questions_to_quiz_round(self):
         factory = QuestionFactory()
         questions = [
                 factory.create_question(
@@ -47,24 +47,24 @@ class TestRounds:
                 possible_answers = ["London", "Paris", "Berlin", "Madrid"]
                 ),
                 factory.create_question(
-                q_type="Written",
+                q_type="Text",
                 question_text="What is the capital of Japan",
                 correct_answer="Tokyo"
                 )
                 ]
-        round = Round(
+        quiz_round = Round(
                 category_title="Geography"
                )
 
         for question in questions:
-            round.add_question(question)
+            quiz_round.add_question(question)
                 
         for question in questions:
-            assert question in round.questions
+            assert question in quiz_round.questions
 
-        assert len(round.questions) == len(questions)
+        assert len(quiz_round.questions) == len(questions)
 
-    def test_removing_question_from_round(self):
+    def test_removing_question_from_quiz_round(self):
         factory = QuestionFactory()
         questions = [
                 factory.create_question(
@@ -92,25 +92,25 @@ class TestRounds:
                 possible_answers = ["London", "Paris", "Berlin", "Madrid"]
                 ),
                 factory.create_question(
-                q_type="Written",
+                q_type="Text",
                 question_text="What is the capital of Japan",
                 correct_answer="Tokyo"
                 )
                 ]
-        round = Round(
+        quiz_round = Round(
                 category_title="Geography"
                )
 
         for question in questions:
-            round.add_question(question)
+            quiz_round.add_question(question)
                 
-        removed_question = round.remove_question(question_number=1)
+        removed_question = quiz_round.remove_question(question_number=1)
 
         assert removed_question.category_title == "What is the capital of France"
 
-        assert len(round.questions) == len(questions) - 1
+        assert len(quiz_round.questions) == len(questions) - 1
 
-    def test_getting_question_based_on_position(self):
+    def test_getting_question_based_on_index(self):
         factory = QuestionFactory()
         questions = [
                 factory.create_question(
@@ -138,19 +138,19 @@ class TestRounds:
                 possible_answers = ["London", "Paris", "Berlin", "Madrid"]
                 ),
                 factory.create_question(
-                q_type="Written",
+                q_type="Text",
                 question_text="What is the capital of Japan",
                 correct_answer="Tokyo"
                 )
                 ]
-        round = Round(
+        quiz_round = Round(
                 category_title="Geography"
                )
 
         for question in questions:
-            round.add_question(question)
+            quiz_round.add_question(question)
             
-        q = round.get_question(position=1)
+        q = quiz_round.get_question(index=1)
 
         assert q.question_text == "What is the capital of France"
 
@@ -182,19 +182,19 @@ class TestRounds:
                 possible_answers = ["London", "Paris", "Berlin", "Madrid"]
                 ),
                 factory.create_question(
-                q_type="Written",
+                q_type="Text",
                 question_text="What is the capital of Japan",
                 correct_answer="Tokyo"
                 )
                 ]
-        round = Round(
+        quiz_round = Round(
                 category_title="Geography"
                )
 
         for question in questions:
-            round.add_question(question)
+            quiz_round.add_question(question)
 
         with pytest.raises(IndexError, match="Question not in Round"):
-            q = round.get_question(position=6)
+            q = quiz_round.get_question(index=6)
 
 
