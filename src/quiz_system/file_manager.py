ed3c4646259d2dd651c6b508fec3435013fc9328
SamWatt5 <2508240@dundee.ac.uk>
2026-03-13 15:05:16 +0000

    writted load to file function.. idk if it works

--- src/quiz_system/file_manager.py
+++ src/quiz_system/file_manager.py
@@ -1,5 +1,109 @@
 import json
+from .quiz import Quiz
+from .round import Round
+from question_factory import *
 
 class FileManager:
-    def load_from_file(self, filename):
-        file_json = json.load(filename)
+    
+    def load_quizzes_from_file(self, filename):
+        """
+        Loads quiz from json file.
+        File must be in the following format:
+
+        {
+            "quizzes": [
+            {
+                "quiz_title": "Quiz 1",
+                "quiz_author"; "John Pork"
+                "rounds": [
+                    {
+                        "round_title": "Geography",
+                        "questions": [
+                            {
+                                "question_type": "Text",
+                                "question_text": "What is the capital of France",
+                                "correct_answer": "Paris",
+                                "scoring_strategy": "StandardScore"
+                            },
+                            {
+                                "question_type": "MultipleChoice",
+                                "question_text": "What is the capital of Japan",
+                                "correct_answer": "Tokyo"
+                                "possible_answers": [
+                                    "Kyoto",
+                                    "Lijang",
+                                    "Tokyo",
+                                    "Hanamura"
+                                ],
+                                "scoring_strategy": "BonusScore"
+                            },
+                        ]
+                    },
+                    {
+                        "round_title": "Maths",
+                        "questions": [
+                            {
+                                "question_type": "Text",
+                                "question_text": "What is the 2+2",
+                                "correct_answer": "4",
+                                "scoring_strategy": "PenaltyScore"
+                            },
+                            {
+                                "question_type": "MultipleChoice",
+                                "question_text": "What is the 3-1",
+                                "correct_answer": "2"
+                                "possible_answers": [
+                                    "2",
+                                    "4",
+                                    "3",
+                                    "1"
+                                ],
+                                "scoring_strategy": "StandardScore"
+                            },
+                        ]
+                    }
+                ]
+            },
+            ]
+        }
+        """
+        data = None
+        with open(filename, "r") as file:
+            data = json.load(file)
+        if "quizzes" not in data:
+            return TypeError("Incorrect JSON format")
+        
+        quizzes_dict_list = data.quizzes
+
+        quizzes_quiz = []
+        for i, quiz_dict in enumerate(quizzes_dict_list):
+            quizzes_quiz.append(Quiz(quiz_dict.quiz_title, quiz_dict.quiz_author))
+
+            quiz_rounds = []
+            for j,  r in enumerate(quiz_dict.rounds):
+                quiz_rounds.append(Round(r.round_title))
+
+                round_questions = []
+                for k, q in enumerate(r.questions):
+                    factory = QuestionFactory()
+
+                    round_questions.append(factory.create_question(
+                        q_type = q.question_type,
+                        question_text = q.question_text,
+                        correct_answer = q.correct_answer,
+                        scoring_strategy = q.scoring_strategy,
+                        possible_answer = q.possible_answers if q.possible_answers else None
+                        ))
+                    
+                quiz_rounds[j].add_question(round_questions)
+
+            quizzes_quiz[i].add_rounds(quiz_rounds)
+
+        return quizzes_quiz
+    
+    def save_quiz_to_file(self, filename):
+        json_data = {
+            
+        }
+        with open(filename, "w") as file:
+            file.write(json_data)
