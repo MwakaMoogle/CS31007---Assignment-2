a05b15c4145ea7250d91afefb9cd409332f99183
SamWatt5 <2508240@dundee.ac.uk>
2026-03-14 16:18:11 +0000

    GameSession class docstringed

--- src/quiz_system/game_session.py
+++ src/quiz_system/game_session.py
@@ -1,8 +1,18 @@
 from .quiz import Quiz
 from .team import Team
 class GameSession:
-    
+    """
+    Class for a Game Session, handling a quiz and teams
+    """
+
     def __init__(self, current_quiz: Quiz, participating_teams: list[Team], current_round_index: int = 0):
+        """
+        Constructor for GameSession class
+
+        :param current_quiz: quiz to be played
+        :param participating_teams: list of teams playing the quiz
+        :param current_round_index: the round to start on
+        """
         if not isinstance(current_quiz, Quiz):
             raise TypeError("currect quiz must be of type Quiz")
         if not isinstance(participating_teams, list):
@@ -13,15 +23,31 @@ class GameSession:
         self.current_round_index = current_round_index
 
     def start_session(self):
+        """
+        resets the quiz back to the first round
+        """
         self.current_round_index = 0 
 
     def enter_score(self, team, points):
+        """
+        adds the points for each round to each team
+        
+        :param team: The team to add points to
+        :param points: the points being added to the team's score
+        """
         if team not in self.participating_teams:
             raise ValueError("This team is not in this session")
         
         team.add_points(points)
 
     def calculate_team_score(self, team: Team, round_index: int, answer_correct_list: list[bool]):
+        """
+        calculates a team's score for a round, given a list of correct/incorrect, using each question's scoring strategy
+
+        :param team: The team of which a score is being calculated
+        :param round_index: The index of the quiz's rounds list, which is being calculated
+        :param answer_correct_list: list of boolean for each answer wether they were correct or not
+        """
         if not isinstance(team, Team):
             raise TypeError("team must be of type Team")
         if not isinstance(round_index, int):
@@ -33,12 +59,18 @@ class GameSession:
             self.enter_score(team, question.scoring_strategy.execute_scoring(answer_correct_list[i]))
 
     def get_leaderboard(self):
+        """
+        :returns: a sorted leaderboard of teams in the game session
+        """
         return sorted(
             self.participating_teams, 
             key=lambda team: team.get_score(), 
             reverse=True )
     
     def display_leaderboard(self):
+        """
+        displays the game session's leaderboard
+        """
         leaderboard = self.get_leaderboard()
 
         for i, team in enumerate(leaderboard, start=1):
