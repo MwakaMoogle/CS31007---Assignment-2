0366b1930e58f5895e4b7368ef473b40c8024522
SamWIsALegend <notchnotch5@gmail.com>
2026-03-15 18:16:30 +0000

    completed code review for game session, round and team

--- src/quiz_system/team.py
+++ src/quiz_system/team.py
@@ -1,26 +1,42 @@
 class Team:
+    """
+    Class containing methods and variables relating to teams
+    """
+
     def __init__(self, team_name: str):
+        """
+        Constructor for Team class
+
+        :param team_name: name of team
+        """
         self.team_name = team_name
         self.current_score = 0
-        self.match_history = []
 
     def add_points(self, points):
+        """
+        adds points to team score
+
+        :param points: how many points to add to the team
+        """
         if not isinstance(points, int):
             raise TypeError("points must be an integer")
-        
+
         self.current_score += points
 
-        if self.current_score < 0:
-            self.current_score = 0
-
-        self.match_history.append(points)
-
-
     def get_score(self):
+        """
+        :returns: current score of team
+        """
         return self.current_score
 
     def reset_score(self):
-        self.current_score = 0 
+        """
+        Resets score to zero
+        """
+        self.current_score = 0
 
     def get_name(self):
+        """
+        :returns: team name
+        """
         return self.team_name
